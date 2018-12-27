936136466bf515cecde757763589498f665526ee
samaravadi <46079553+samaravadi@users.noreply.github.com>
2018-12-27 15:01:50 -0600

    Update ch12.js

--- verses_text/ch12.js
+++ verses_text/ch12.js
@@ -4,7 +4,7 @@
 // This file holds the verses and translations of chapter twelve from the Bhagavad Gita
 //
 // @author Vivek Bhookya
-// @author
+// @author Sankhya Amaravadi
 // If you edit this file, add yourself as an author!
 
 "12" : {
@@ -12,20 +12,20 @@
   "2" : ["sri-bhagavan uvaca\mayy avesya mano ye mam nitya-yukta upasate\sraddhaya parayopetas te me yuktatama matah (12.02)", "The Supreme Lord said: Those who fix their minds on Me, most integrated,\worshipping Me filled with supreme faith (supreme Sraddha, transcendental\faith) are, in My opinion, the best among yogis."],
   "3" : ["ye tv aksaram anirdesyam avyaktam paryupasate\sarvatra-gam acintyam ca kuta-stham acalam dhruvam (12.03)", "Those who worship the Undefined, the Imperishable Unmanifested, the All-\pervading and Unthinkable, the Unchanging, the Permanent and Eternal,"],
   "4" : ["sanniyamyendriya-gramam sarvatra sama-buddhayah\te prapnuvanti mam eva sarva-bhuta-hite ratah (12.04)", "Having restrained all the senses, in all things equal-minded, concerned with\the welfare of others, they also attain Me."],
-  "5" : ["kleso 'dhikataras tesam avyaktasakta-cetasam\vyakta hi gatir duhkham dehavadbhir avapyate (12.05)", ""],
-  "6" : ["", ""],
-  "7" : ["", ""],
-  "8" : ["", ""],
-  "9" : ["", ""],
-  "10" : ["", ""],
-  "11" : ["", ""],
-  "12" : ["", ""],
-  "13" : ["", ""],
-  "14" : ["", ""],
-  "15" : ["", ""],
-  "16" : ["", ""],
-  "17" : ["", ""],
-  "18" : ["", ""],
-  "19" : ["", ""],
-  "20" : ["", ""]
+  "5" : ["kleso 'dhikataras tesam avyaktasakta-cetasam\vyakta hi gatir duhkham dehavadbhir avapyate (12.05)", "But, greater is the difficulty of those whose minds cling to the\Unmanifested; for the goal of the Unmanifested is hard for the embodied\being to reach."],
+  "6" : ["ye tu sarvani karmani mayi sannyasya mat-parah\ananyenaiva yogena mam dhyayanta upasate (12.06)", "However, those who renounce their every action to Me, solely intent on Me\as the Supreme Goal, and worship Me, meditating on Me, with steadfast\devotion,"],
+  "7" : ["tesam aham samuddharta mrtyu-samsara-sagarat\bhavami na cirat Partha mayy avesita-cetasam (12.07)", "These I will soon lift up on high out of the ocean of the mortal samsara (the\round of birth, death and rebirth), O Partha (Arjuna), for their minds are\fixed on Me."],
+  "8" : ["mayy eva mana adhatsva mayi buddhim nivesaya\nivasisyasi mayy eva ata urdhvam na samsayah (12.08)", "Fix your mind on Me only, and let your intellect dwell within Me; then in\very truth (eva) you will abide hereafter in Me alone."],
+  "9" : ["atha cittam samadhatum na saknosi mayi sthiram\abhyasa-yogena tato mam icchaptum Dhananjaya (12.09)", "But, if you are unable to fix firmly your mind (citta) on Me, then long to\reach Me by the practise of Abhyasa-yoga (any other spiritual discipline and\exercises for self-realisation, also the act of listening to the teaching of the srutis\(sacred knowledge, especially the Upanishads) with a view to obtain knowledge;\or, the practise of dhyana with a firm resolve.), O Dhananjaya (Arjuna)."],
+  "10" : ["abhyase 'py asamartho 'si mat-karma-paramo bhava\mad-artham api karmani kurvan siddhim avapsyasi (12.10)", "If you are incapable even to practise Abhyasa-yoga, then work and act for\Me, making this your intent. Even by performing all actions for Me, you\will attain perfection."],
+  "11" : ["athaitad apy asakto 'si kartum mad-yogam asritah\sarva-karma-phala-tyagam tatah kuru yatatmavan (12.11)", "Then, even if you are unable to do this, then gird up your loins in devotion\for Me alone (reading udygam with some MSS for mad-yogam), and self-\controlled, renounce the fruits of your actions."],
+  "12" : ["sreyo hi jnanam abhyasaj jnanad dhyanam visisyate\dhyanat karma-phala-tyagas tyagac chantir anantaram (12.12)", "For better is knowledge than mere practise, better than knowledge is\meditation; and better than meditation is the renunciation of the fruits of\actions: renunciation leads immediately to peace."],
+  "13" : ["advesta sarva-bhutanam maitrah karuna eva ca\nirmamo nirahankarah sama-duhkha-sukhah ksami (12.13)", "Let him feel hatred for no contingent being, but be friendly and\compassionate, without attachment and egoism, remaining the same in\pleasure as in pain, and forbearing."],
+  "14" : ["santustah satatam yogi yatatma drdha-niscayah\mayy arpita-mano-buddhir yo mad-bhaktah sa me priyah (12.14)", "Ever content and integrated, self-controlled, possessed with solid\conviction, with mind and intellect steeped in Me, he, My devotee, is dear to\Me."],
+  "15" : ["yasman nodvijate loko lokan nodvijate ca yah\harsamarsa-bhayodvegair mukto yah sa ca me priyah (12.15)", "He, by whom the world is not afflicted, and who does not afflict the world,\who is free from exaltation, envy, fear and disquiet, he is likewise dear to\Me."],
+  "16" : ["anapeksah sucir daksa dasino gata-vyathah\sarvarambha-parityagi yo mad-bhaktah sa me priyah (12.16)", "He who is unconcerned, pure, skilled, indifferent, who is not worrying,\renouncing every selfish initiative, he, My devotee, is dear to Me."],
+  "17" : ["yo na hrsyati na dvesti na socati na kanksati\subhasubha-parityagi bhaktiman yah sa me priyah (12.17)", "He who neither rejoices, nor hates, nor grieves, nor desires, renouncing\fortune and misfortune, who is full of devotion (bhaktimat), he is dear to Me."],
+  "18" : ["samah satrau ca mitre ca tatha manapamanayoh\sitosna-sukha-duhkhesu samah sanga-vivarjitah (12.18)", "He who is alike to enemies and friends, as well as towards honour and\dishonour, alike in cold and heat, in pleasure as in pain, and who is free\from attachment,"],
+  "19" : ["tulya-ninda-stutir mauni santusto yena kenacit\aniketah sthira-matir bhaktiman me priyo narah (12.19)", "Unmoved by praise and blame, who is silent, homeless, satisfied with\whatever comes his way, steady-minded, full of devotion, that man is dear\to Me."],
+  "20" : ["ye tu dharmamrtam idam yathoktam paryupasate\sraddadhana mat-parama bhaktas te 'tiva me priyah (12.20)", "But, those who partake in this life-giving knowledge (amrita- dharma) as\taught above, full of faith, making Me their goal, and devoted, they areespecially dear to Me."]
 }
