b9025fbaeef1d1ef5804696f0a84e835a97de1b0
samaravadi <46079553+samaravadi@users.noreply.github.com>
2018-12-27 14:07:17 -0600

    working on chapter 12

--- verses_text/ch12.js
+++ verses_text/ch12.js
@@ -8,11 +8,11 @@
 // If you edit this file, add yourself as an author!
 
 "12" : {
-  "1" : ["", ""],
-  "2" : ["", ""],
-  "3" : ["", ""],
-  "4" : ["", ""],
-  "5" : ["", ""],
+  "1" : ["arjuna uvaca\evam satata-yukta ye bhaktas tvam paryupasate\ye capy aksaram avyaktam tesam ke yoga-vittamah (12.01)", "Arjuna said: Those devotees who are ever integrated, and serve You with\unswerving devotion (bhakta), and others who worship the Imperishable\Unmanifested, which of them are better knowers of Yoga?"],
+  "2" : ["sri-bhagavan uvaca\mayy avesya mano ye mam nitya-yukta upasate\sraddhaya parayopetas te me yuktatama matah (12.02)", "The Supreme Lord said: Those who fix their minds on Me, most integrated,\worshipping Me filled with supreme faith (supreme Sraddha, transcendental\faith) are, in My opinion, the best among yogis."],
+  "3" : ["ye tv aksaram anirdesyam avyaktam paryupasate\sarvatra-gam acintyam ca kuta-stham acalam dhruvam (12.03)", "Those who worship the Undefined, the Imperishable Unmanifested, the All-\pervading and Unthinkable, the Unchanging, the Permanent and Eternal,"],
+  "4" : ["sanniyamyendriya-gramam sarvatra sama-buddhayah\te prapnuvanti mam eva sarva-bhuta-hite ratah (12.04)", "Having restrained all the senses, in all things equal-minded, concerned with\the welfare of others, they also attain Me."],
+  "5" : ["kleso 'dhikataras tesam avyaktasakta-cetasam\vyakta hi gatir duhkham dehavadbhir avapyate (12.05)", ""],
   "6" : ["", ""],
   "7" : ["", ""],
   "8" : ["", ""],
