1443da79f1ef7ec6429d7e6f2e477319a6fb12a1
uzhshah40 <46080505+uzhshah40@users.noreply.github.com>
2018-12-30 15:45:49 -0600

    Update ch15.js

--- verses_text/ch15.js
+++ verses_text/ch15.js
@@ -8,13 +8,13 @@
 // If you edit this file, add yourself as an author!
 
 "15" : {
-  "1" : ["sri-bhagavan uvaca/nurdhva-mulam adhah-sakham asvattham prahur avyaya/nchandamsi yasya parnani yas tarn veda sa veda-vit (15.1)","]
-  "2" : ["", ""],
-  "3" : ["", ""],
-  "4" : ["", ""],
-  "5" : ["", ""],
-  "6" : ["", ""],
-  "7" : ["", ""],
+  "1" : ["sri-bhagavan uvaca/nurdhva-mulam adhah-sakham asvattham prahur avyaya/nchandamsi yasya parnani yas tarn veda sa veda-vit (15.1)","The Supreme Lord said: With roots above and branches beneath, the/nAsvattha (fig tree, banyan tree) is said to be imperishable; its leaves are the/nVedic hymns; he who knows it, knows the Vedas."],  
+  "2" : ["adhas cordhvam prasrtas tasya sakha guna-pravrddha visaya-pravalah/nadhas ca mulany anusantatani karmanubandhini manusya-loke (15.2)","Below and above are spread its branches, nourished by the constituents/n(Gunas of nature, Qualities); sense-objects are its buds, and below its roots/nproliferate inseparably linked with actions in the world of men."],
+  "3" : ["na rupam asyeha tathopalabhyate nanto na cadir na ca sampratistha/nasvattham enam su-virudha-mulam asanga-sastrena drdhena chittva (15.3)","No form of it can here be perceived, nor its end, nor its origin, nor its/nexistence. This strongly-rooted Asvattha (fig tree, banyan tree) having been/ncut down by an unswerving sword of non-attachment;"],
+  "4" : ["tatah padam tat parimargitavyam yasmin gata na nivartanti bhuyah/ntarn eva cadyam purusam prapadye yatah pravrttih prasrta purani (15.4)","Then, 'That' (tat) Goal should be sought, to which, when having tread on,/nthere is no return (saying): I take refuge in that Primeval Purusha (Person,/nMan, Principle) from whom flowed forth this ancient cosmic process"],
+  "5" : ["nirmana-mohajita-sanga-dosa adhyatma-nitya vinivrtta-kamah/ndvandvair vimuktah sukha-duhkha-samjnair gacchanty amudhah padam/navyayam tat (15.5)","Free from pride and ignorance, victorious over the vice of attachment, ever/nabiding in what concerns the Self, desire suppressed, released from the/npairs of opposites (all dualities) made known in pleasure as in pain, they/ntread, undeluded, that Eternal Goal."],  
+  "6" : ["na tad bhasayate suryo na sasanko na pavakah/nyad gatva na nivartante tad dhama paramam mama (15.6)","Nor does the sun lights there, nor moon, nor fire. That is My supreme/n  abode (dhama, home) from where, having reached it, no one returns."],
+  "7" : ["mamaivamso jiva-loke jiva-bhutah sanatanah/nmanah-sasthanindriyani prakrti sthani karsati (15.7)","A portion (a minute particle, a ray) of Mine own Self, which becomes life in/nthe living world (the living world of Jivas) into an immortal Jiva (Jivatma,/nSoul; a life, individualised from the Supreme (Eternal) Being), drawing to itself/nthe senses, with the mind as the sixth, abiding in Prakriti (matter)."],
   "8" : ["", ""],
   "9" : ["", ""],
   "10" : ["", ""],
