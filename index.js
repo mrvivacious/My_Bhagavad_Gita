c1da9194289434b8f4a0257c5a88213a085ef4d4
Vivek Bhookya <v.bhookya98@gmail.com>
2023-11-06 10:58:57 -0600

    [alexa] copy over code from aws lambda (prod)

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const AUDIO_URL = 'https://raw.githubusercontent.com/mrvivacious/My_Bhagavad_Git
 
 const SKILL_NAME = 'My Bhagavad Gita';
 const HELP_MESSAGE = 'To use this software, say something like, play chapter one, verse one, or say, play a verse about confusion.';
-const HELP_REPROMPT = 'Say both a chapter number and a verse number, or ask to play a verse about a topic.';
+const HELP_REPROMPT = 'Say both a chapter number and a verse number, or ask to play a verse about a topic, or ask for a random verse.';
 const STOP_MESSAGE = 'Goodbye!';
 
 //============ HELPER FUNCTIONS ===========================================================================================================
@@ -100,18 +100,18 @@ function constructAudioURL(chapter, verse) {
 }
 
 //=========================================================================================================================================
-
+                                                        
 const handlers = {
   'SessionEndedRequest': function () {
     this.emit(':responseReady');
   },
-  'LaunchRequest': function () {
+  'LaunchRequest': function() {
     // hello world
     // Thank you, https://medium.freecodecamp.org/amazon-has-made-it-easier-to-add-sounds-to-custom-alexa-skills-513b865d7528
 
-    let launchText = 'Welcome to the Bhagavad Gita. There are two actions: ' +
-      'request a verse by chapter and verse numbers, and request a verse by topic.';
-    launchText += ' ' + HELP_MESSAGE;
+    let launchText = 'Welcome to the Bhagavad Gita. You can ' +
+    'request a verse by chapter and verse numbers, request a verse by topic, or ask for a random verse.';
+    // launchText += ' ' + HELP_MESSAGE;
 
     this.response.cardRenderer(SKILL_NAME, launchText);
     this.response.speak(launchText).listen(HELP_REPROMPT);
@@ -123,13 +123,10 @@ const handlers = {
     let chapterNumber = this.event.request.intent.slots.chapter.value;
     let verseNumber = this.event.request.intent.slots.verse.value;
 
-    // Validate chapter and verse are not undefined
-    // The user provided both values
+    // Validate chapter and verse are defined
     if (chapterNumber && verseNumber) {
-      // When both are non-null, ensure the verse is in the bounds of the
-      //  desired chapter
+      // Ensure chapter, verse are within the Gita
       if (validateBounds(chapterNumber, verseNumber)) {
-        // If true, we have a valid verse, deliver the audio URL
         let audioURL = constructAudioURL(chapterNumber, verseNumber);
         audioURL = `<audio src='${audioURL}'/>`;
 
@@ -143,7 +140,7 @@ const handlers = {
       //  the chapter instead has some number of verses
       else {
         let output;
-
+        
         // Is chapter even valid?
         if (chapterNumber < 1 || chapterNumber > 18) {
           output = 'The Bhagavad Gita has 18 chapters, please request a chapter between ' +
@@ -152,12 +149,12 @@ const handlers = {
         else {
           // Number of verses in the selected chapter
           let versesInChapter = Object.keys(BHAGAVAD_GITA[chapterNumber]).length;
-
+  
           output = 'Hmm, chapter ' + chapterNumber + ' does not have a verse ' +
             verseNumber + '. Chapter ' + chapterNumber + ' has ' + versesInChapter +
             ' verses.';
         }
-
+        
         this.response.cardRenderer(SKILL_NAME, output);
         this.response.speak(output);
         this.emit(':responseReady');
@@ -175,7 +172,7 @@ const handlers = {
       this.emit(':responseReady');
     }
   },
-  'TopicEnquiryIntent': function () {
+  'TopicEnquiryIntent': function() {
     // Get the user's requested topic
     let topic = this.event.request.intent.slots.topic.value.toLowerCase();
 
@@ -183,6 +180,15 @@ const handlers = {
     // Collect the list of verses that corresponds to this topic
     // let topicVerses = TOPICS_MAP[topic];
     let topicVerses = getTopic(topic);
+    
+    if (!topicVerses) {
+      let output = `We haven't found a verse about '${topic}'. The developer will take a closer look for relevant verses and release them in a future update.`; 
+      output += ' Meanwhile, you can see supported topics in this skill\'s description page in the app store page.';
+
+    this.response.cardRenderer(SKILL_NAME, output.split(':')[0]);
+    this.response.speak(output);
+    this.emit(':responseReady');
+    }
 
     // console.log("--- OUR VERSES ---");
     // console.log(topicVerses);
@@ -221,13 +227,13 @@ const handlers = {
     this.response.speak(output);
     this.emit(':responseReady');
   },
-  'RandomVerseIntent': function () {
+  'RandomVerseIntent': function() {
     // Get random chapter
     // + 1 because the Gita is indexed 1 - 18
     let randomChapter = Math.floor(Math.random() * 18) + 1;
     let chapter = BHAGAVAD_GITA[randomChapter];
     let chapterLength = Object.keys(chapter).length;
-
+    
     // Get random verse from that chapter
     let randomVerse = Math.floor(Math.random() * chapterLength) + 1;
 
@@ -241,26 +247,26 @@ const handlers = {
     this.response.speak(output);
     this.emit(':responseReady');
   },
-  'AMAZON.HelpIntent': function () {
+  'AMAZON.HelpIntent': function() {
     const speechOutput = HELP_MESSAGE;
     const reprompt = HELP_REPROMPT;
 
     this.response.speak(speechOutput).listen(reprompt);
     this.emit(':responseReady');
   },
-  'AMAZON.CancelIntent': function () {
+  'AMAZON.CancelIntent': function() {
     this.response.speak(STOP_MESSAGE);
     this.emit(':responseReady');
   },
-  'AMAZON.StopIntent': function () {
+  'AMAZON.StopIntent': function() {
     this.emit(':responseReady');
   },
-  'AMAZON.FallbackIntent': function () {
+  'AMAZON.FallbackIntent': function() {
     this.emit(':responseReady');
   }
 };
 
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
   const alexa = Alexa.handler(event, context, callback);
   alexa.APP_ID = APP_ID;
   alexa.registerHandlers(handlers);
@@ -269,52 +275,52 @@ exports.handler = function (event, context, callback) {
 
 
 
-// In the even where users provided one value at a time
-// Let's just go ALL - or - NOTHING
-// ONE VALUE //
-// Verse is defined, chapter == undefined, number == undefined
-// + Prompt for the missing chapter
-// If we already have a value saved for this session, don't overwrite
-// IE. "Verse three" response will set chapterNumber to undefined
-// if (verseNumber && !chapterNumber && !number) {
-//   this.attributes["verse"] = verseNumber;
+ // In the even where users provided one value at a time
+ // Let's just go ALL - or - NOTHING
+ // ONE VALUE //
+    // Verse is defined, chapter == undefined, number == undefined
+    // + Prompt for the missing chapter
+    // If we already have a value saved for this session, don't overwrite
+    // IE. "Verse three" response will set chapterNumber to undefined
+    // if (verseNumber && !chapterNumber && !number) {
+    //   this.attributes["verse"] = verseNumber;
 
-//   let output = 'What number chapter do you want to hear verse ' + verseNumber + 'from?';
-//   let listen = 'Say chapter, followed by the chapter number';
+    //   let output = 'What number chapter do you want to hear verse ' + verseNumber + 'from?';
+    //   let listen = 'Say chapter, followed by the chapter number';
 
-//   this.response.cardRenderer(SKILL_NAME, output);
-//   this.response.speak(output).listen(listen);
-//   this.emit(':responseReady');
-// }
+    //   this.response.cardRenderer(SKILL_NAME, output);
+    //   this.response.speak(output).listen(listen);
+    //   this.emit(':responseReady');
+    // }
 
-// Chapter is defined, verse == undefined, number == undefined
-// if (chapterNumber && !verseNumber && !number) {
-//   this.attributes["chapter"] = chapterNumber;
+    // Chapter is defined, verse == undefined, number == undefined
+    // if (chapterNumber && !verseNumber && !number) {
+    //   this.attributes["chapter"] = chapterNumber;
 
-//   let output = 'From chapter ' + chapterNumber + ', what number verse do you want to hear?';
-//   let listen = 'Say verse, followed by the verse number';
+    //   let output = 'From chapter ' + chapterNumber + ', what number verse do you want to hear?';
+    //   let listen = 'Say verse, followed by the verse number';
 
-//   this.response.cardRenderer(SKILL_NAME, output);
-//   this.response.speak(output).listen(listen);
-//   this.emit(':responseReady');
-// }
+    //   this.response.cardRenderer(SKILL_NAME, output);
+    //   this.response.speak(output).listen(listen);
+    //   this.emit(':responseReady');
+    // }
 
-// // NUMBER RESPONSE //
-// // Verse is defined, chapter == undefined, number == defined
-// // + Set the missing attribute with ${number} and proceed
-// if (verseNumber && !chapterNumber && number) {
-//   // VALIDATE BOUNDS
+    // // NUMBER RESPONSE //
+    // // Verse is defined, chapter == undefined, number == defined
+    // // + Set the missing attribute with ${number} and proceed
+    // if (verseNumber && !chapterNumber && number) {
+    //   // VALIDATE BOUNDS
 
-//   // this.response.cardRenderer(SKILL_NAME, output);
-//   // this.response.speak(output).listen(listen);
-//   // this.emit(':responseReady');
-// }
+    //   // this.response.cardRenderer(SKILL_NAME, output);
+    //   // this.response.speak(output).listen(listen);
+    //   // this.emit(':responseReady');
+    // }
 
-// // Chapter is defined, verse == undefined, number == defined
-// if (chapterNumber && !verseNumber && number) {
-//   // VALIDATE BOUNDS
+    // // Chapter is defined, verse == undefined, number == defined
+    // if (chapterNumber && !verseNumber && number) {
+    //   // VALIDATE BOUNDS
 
-//   // this.response.cardRenderer(SKILL_NAME, output);
-//   // this.response.speak(output).listen(listen);
-//   // this.emit(':responseReady');
-// }
+    //   // this.response.cardRenderer(SKILL_NAME, output);
+    //   // this.response.speak(output).listen(listen);
+    //   // this.emit(':responseReady');
+    // }
